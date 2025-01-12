d27c23f7d6e605557f2bd19ae3eea24e700caa8b
solutena <solutena@users.noreply.github.com>
2025-01-12 18:47:09 +0900

    1.0.2v 업데이트

--- Runtime/FileManager.cs
+++ Runtime/FileManager.cs
@@ -1,74 +1,19 @@
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using UnityEngine;
 
-public class FileManager : MonoBehaviour
+public class FileManager
 {
-	public static FileManager Instance { get; set; }
-
-	public List<PathSetting> pathSettings;
-	private ReadOnlyDictionary<string, string> Paths { get; set; }
-
-	[Serializable]
-	public class PathSetting
+	public static void ExportJson<T>(T data, string directory) => ExportJson(data, directory, typeof(T).FullName);
+	public static void ExportJson<T>(T data, string directory, string fileName)
 	{
-		public enum PathType
-		{
-			dataPath,               // 주 데이터 저장 경로
-			persistentDataPath,     // 지속 가능한 데이터 저장 경로
-			streamingAssetsPath,    // 스트리밍 자산 경로
-			temporaryCachePath,     // 임시 캐시 경로
-			consoleLogPath,         // 콘솔 로그 경로
-		}
-
-		public PathType type;
-		public string key;
-		public string path;
-
-		public string FullPath
-		{
-			get
-			{
-				return type switch
-				{
-					PathType.dataPath => Path.Combine(Application.dataPath, path),
-					PathType.persistentDataPath => Path.Combine(Application.persistentDataPath, path),
-					PathType.streamingAssetsPath => Path.Combine(Application.streamingAssetsPath, path),
-					PathType.temporaryCachePath => Path.Combine(Application.temporaryCachePath, path),
-					PathType.consoleLogPath => Path.Combine(Application.consoleLogPath, path),
-					_ => throw new NotImplementedException(),
-				};
-			}
-		}
-	}
-
-	private void Awake()
-	{
-		Instance = this;
-		Paths = new(pathSettings.ToDictionary(setting => setting.key, setting => setting.FullPath));
-	}
-
-	private string GetPath(string pathKey, string fileName)
-	{
-		var directory = Paths.GetValueOrDefault(pathKey);
-		if (string.IsNullOrEmpty(directory))
-			throw new ArgumentException($"Path type '{pathKey}' is not valid or not defined.");
-		return Path.Combine(directory, fileName);
-	}
-
-	public void ExportJson<T>(T data, string pathKey) => ExportJson(data, pathKey, typeof(T).FullName);
-	public void ExportJson<T>(T data, string pathKey, string fileName)
-	{
-		string path = GetPath(pathKey, $"{fileName}.json");
+		string path = Path.Combine(directory, $"{fileName}.json");
 		try
 		{
-			Directory.CreateDirectory(pathKey);
+			Directory.CreateDirectory(directory);
 			string json = JsonUtility.ToJson(data, true);
 			File.WriteAllText(path, json, Encoding.UTF8);
 			Debug.Log($"Exported JSON for {fileName} to: {path}");
@@ -79,10 +24,10 @@ public class FileManager : MonoBehaviour
 		}
 	}
 
-	public T ImportJson<T>(string pathKey) => ImportJson<T>(pathKey, typeof(T).FullName);
-	public T ImportJson<T>(string pathKey, string fileName)
+	public static T ImportJson<T>(string directory) => ImportJson<T>(directory, typeof(T).FullName);
+	public static T ImportJson<T>(string directory, string fileName)
 	{
-		string path = GetPath(pathKey, $"{fileName}.json");
+		string path = Path.Combine(directory, $"{fileName}.json");
 		try
 		{
 			if (File.Exists(path))
@@ -100,13 +45,13 @@ public class FileManager : MonoBehaviour
         }
     }
 
-	public void ExportXmlSchema<T>(string pathKey) where T : new() => ExportXmlSchema<T>(pathKey, typeof(T).FullName);
-	public void ExportXmlSchema<T>(string pathKey, string fileName) where T : new()
+	public static void ExportXmlSchema<T>(string directory) where T : new() => ExportXmlSchema<T>(directory, typeof(T).FullName);
+	public static void ExportXmlSchema<T>(string directory, string fileName) where T : new()
 	{
-		string path = GetPath(pathKey, $"{fileName}_Schema.xml");
+		string path = Path.Combine(directory, $"{fileName}_Schema.xml");
 		try
 		{
-			Directory.CreateDirectory(pathKey);
+			Directory.CreateDirectory(directory);
 			var serializer = new XmlSerializer(typeof(T[]));
 			using var writer = new XmlTextWriter(path, Encoding.UTF8)
 			{
@@ -122,10 +67,10 @@ public class FileManager : MonoBehaviour
 		}
 	}
 
-	public T[] ImportXml<T>(string pathKey) => ImportXml<T>(pathKey, typeof(T).FullName);
-	public T[] ImportXml<T>(string pathKey, string fileName)
+	public static T[] ImportXml<T>(string directory) => ImportXml<T>(directory, typeof(T).FullName);
+	public static T[] ImportXml<T>(string directory, string fileName)
 	{
-		string path = GetPath(pathKey, $"{fileName}.xml");
+		string path = Path.Combine(directory, $"{fileName}.xml");
 		try
 		{
 			if (File.Exists(path))
